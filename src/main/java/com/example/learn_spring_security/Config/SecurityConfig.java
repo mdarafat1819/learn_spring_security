256c0baa89382922d1072df64b406a6a6c1adc1d
Yeasin <mdarafat1819@gmail.com>
2026-01-28 15:57:13 +0600

    set up a custome UserDetailsService

--- src/main/java/com/example/learn_spring_security/Config/SecurityConfig.java
+++ src/main/java/com/example/learn_spring_security/Config/SecurityConfig.java
@@ -2,24 +2,11 @@ package com.example.learn_spring_security.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public UserDetailsService UserDetailsServic() {
-        UserDetails user = User.builder()
-            .username("user")
-            .password("$2a$12$LxAfp7HYm95Q4N92lushWeK/uzZUTAsfCbWxSX45Za7rGMgjEoeFS")
-            .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
